66e573f6c3b6aa3288b909816842f5249b58d767
feenry <taylor.wright90@live.com>
2022-07-22 11:55:56 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   mcp3008test2.py

--- mcp3008test2.py
+++ mcp3008test2.py
@@ -4,6 +4,6 @@ from gpiozero import MCP3008
 gaugeIn = MCP3008(0)
 
 while True:
-    print("Raw output: " + gaugeIn.value)   
-    print("not-Raw output: " + int(gaugeIn.value))   
+    print("Raw output: ", gaugeIn.value)   
+    print("not-Raw output: ", int(gaugeIn.value))   
     time.sleep(1)
